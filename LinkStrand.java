3816da99176d31362f25de8ddee065a1f61a5106
Katherine Yu <katherine.l.yu@duke.edu>
2023-10-19 16:27:35 -0400

    trial 1

---
+++ LinkStrand.java
@@ -0,0 +1,107 @@
+public class LinkStrand implements IDnaStrand {
+
+    private Node myFirst, myLast;
+    private long mySize;
+    private int myAppends;
+    private int myIndex;
+    private Node myCurrent;
+    private int myLocalIndex;
+
+    public LinkStrand() {
+        this("");
+    }
+
+    public LinkStrand(String s) {
+        initialize(s);
+    }
+
+    public void initialize(String source) {
+        Node n = new Node(source, null);
+        myFirst = n;
+        myLast = n;
+        myAppends = 0;
+        myIndex = 0;
+        myCurrent = myFirst;
+        myLocalIndex = 0;
+    }
+
+    public LinkStrand getInstance(String source) {
+        return new LinkStrand(source);
+    }
+    
+    public long size() {
+		return mySize;
+	}
+
+    public IDnaStrand append(String dna) {
+        Node newDNA = new Node (dna, null);
+        myLast.next = newDNA;
+        myLast = myLast.next;
+        mySize += dna.length();
+        myAppends++;
+        return this;
+    }
+
+    public int getAppendCount() {
+        return myAppends;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder("");
+        Node myCurrent = myFirst;
+        while (myCurrent != null) {
+            s.append(myCurrent.info);
+            myCurrent = myCurrent.next;
+        }
+        return s.toString();
+    }
+
+    @Override
+    public IDnaStrand reverse() {
+        LinkStrand reversedStrand = new LinkStrand();
+        Node current = myFirst;
+
+        while (current!= null) {
+            StringBuilder s = new StringBuilder(current.info);
+            String reversedString = s.reverse().toString();
+            Node rev = new Node(reversedString); //new node of reversed string
+            if (current == myFirst) {
+                reversedStrand.myLast = rev;
+            }
+            reversedStrand.mySize += rev.info.length(); //update size
+            rev.next = reversedStrand.myFirst;
+            reversedStrand.myFirst = rev;
+            current = current.next;
+
+        }
+        return reversedStrand;
+    }
+
+    public char charAt(int index) {
+        myIndex = index;
+        int characterCount = myCurrent.info.length();
+        while (myCurrent != null) {
+            if (myIndex > characterCount && myIndex < characterCount + myCurrent.info.length()) {
+                myLocalIndex = myIndex - characterCount;
+                return myCurrent.info.charAt(myLocalIndex);
+            }
+            characterCount += myCurrent.info.length();
+            myCurrent = myCurrent.next;
+        }
+        throw new IndexOutOfBoundsException();
+    }
+
+    private class Node {
+        private String info;
+        private Node next;
+
+        private Node(String i) {
+            info = i;
+        }
+
+        private Node(String i, Node n) {
+            info = i;
+            next = n;
+        }
+    }
+}
--- src/DNABenchmark.java
+++ src/DNABenchmark.java
@@ -14,8 +14,8 @@ import java.util.*;
 public class DNABenchmark {
 	
 	// Select/uncomment which implementation you want to benchmark
-	private static final String strandType = "StringStrand";
-	//private static final String strandType = "StringBuilderStrand";
+	//private static final String strandType = "StringStrand";
+	private static final String strandType = "StringBuilderStrand";
 	//private static final String strandType = "LinkStrand";
 	
 	
